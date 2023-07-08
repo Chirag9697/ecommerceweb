c678a18b8909721ce72b4dd7e987ee195bca8a03
chirag <chirru.ra@gmail.com>
2023-07-08 10:48:41 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css'
 import ProductList from './pages/ProductList';
 import Home from './pages/Home'
 // import Navbar from './components/Navbar'
+import Product from './pages/Product';
+import Register from './pages/Register';
 function App() {
   return (
     <>
-      <ProductList/>
+      <Register/>
     </>
   );
 }
